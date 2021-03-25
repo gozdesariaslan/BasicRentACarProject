a896d75670d35e4bfee79d1c1812d5e6e9d11a0a
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-03-25 15:08:52 +0300

    ColorsController added

---
+++ WebAPI/Controllers/ColorsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+         IColorService _colorService;
+
+         public ColorsController(IColorService colorService)
+         {
+             _colorService = colorService;
+         }
+
+         [HttpGet("getall")]
+         public IActionResult Get()
+         {
+             var result = _colorService.GetAll();
+             if (result.Success == true)
+             {
+                 return Ok(result);
+             }
+
+             return BadRequest(result);
+         }
+
+         [HttpPost("add")]
+         public IActionResult Add(Color color)
+         {
+             var result = _colorService.Add(color);
+             if (result.Success == true)
+             {
+                 return Ok(result);
+             }
+
+             return BadRequest(result);
+         }
+
+         [HttpPost("update")]
+         public IActionResult Update(Color color)
+         {
+             var result = _colorService.Update(color);
+             if (result.Success == true)
+             {
+                 return Ok(result);
+             }
+
+             return BadRequest(result);
+         }
+    }
+}
