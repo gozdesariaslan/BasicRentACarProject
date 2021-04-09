be9c7f883c6b8ac1935a219d441982bf358eaa34
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-10 01:43:45 +0300

    cors added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -38,6 +38,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
             //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             //services.AddSingleton<IBrandService, BrandManager>();
@@ -88,6 +89,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
