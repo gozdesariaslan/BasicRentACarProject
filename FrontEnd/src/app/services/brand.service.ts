f92958b480c02e471689e86681bf5361500e7fac
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-10 08:43:06 +0300

    brand component updated

--- FrontEnd/src/app/services/brand.service.ts
+++ FrontEnd/src/app/services/brand.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Brand } from '../models/brand';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
+apiUrl="https://localhost:44317/api/brands/getall";
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  getBrands():Observable<ListResponseModel<Brand>>{
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+  }
 }
