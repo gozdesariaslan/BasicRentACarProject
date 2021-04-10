0367ac09885c28f335c3363977c9c5f5781d71ee
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-10 20:23:41 +0300

    path updated

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:CarComponent},
+  {path:"cars", component:CarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
