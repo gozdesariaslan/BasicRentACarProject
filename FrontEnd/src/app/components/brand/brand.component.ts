8bfa7f823a34663837dcaa217082bafebc5f53d4
Gözde Sarıaslan <sariaslangozde@gmail.com>
2021-04-24 11:32:53 +0300

    brand component updated

--- FrontEnd/src/app/components/brand/brand.component.ts
+++ FrontEnd/src/app/components/brand/brand.component.ts
@@ -10,6 +10,7 @@ import { BrandService } from 'src/app/services/brand.service';
 export class BrandComponent implements OnInit {
 
 brands:Brand[]=[]
+currentBrand:Brand;
   constructor(private brandService:BrandService) { }
 
   ngOnInit(): void {
@@ -20,5 +21,17 @@ brands:Brand[]=[]
     this.brandService.getBrands().subscribe((response)=>{
       this.brands=response.data;
     })
+  }
+
+  setCurrentBrand(brand:Brand){
+    this.currentBrand=brand;
+  }
+
+  getCurrentBrandClass(brand:Brand){
+    if(brand==this.currentBrand){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
   }
 }
